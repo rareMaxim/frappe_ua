12aa6330bd0b2477fadd6f74cfc27514f1417008
Maxim S <maks4a@gmail.com>
2024-01-24 21:07:43 +0200

    wip

---
+++ frappe_ua/katottg/doctype/katottg_item/katottg_item.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Maxim Sysoev and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+
+class KatottgItem(NestedSet):
+	pass
